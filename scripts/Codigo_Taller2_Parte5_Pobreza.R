8128af28f81bbaf0d2bf5fd4759bb4804236a57d
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-02-26 13:06:02 -0500

    Update Codigo_Taller2_Parte5_Pobreza.R
    Arboles

--- scripts/Codigo_Taller2_Parte5_Pobreza.R
+++ scripts/Codigo_Taller2_Parte5_Pobreza.R
@@ -12,12 +12,18 @@
 #  Fecha: 22/02/2023 
 
 
-# - Limpiar espacio de trabajo
-
-rm(list = ls())
-
 # - Librerias y paquetes 
 
 library(pacman)
-p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot, openxlsx, rio)
+p_load(rvest, tidyverse, ggplot2, robotstxt, psych, caret, boot, openxlsx, rio, rpart)
+p_load(rpart.plot)
 
+
+
+# Modelo 1 Arbol simple
+m1p1 <- rpart(Pobre~., 
+               data    = train,
+               method = "class",
+               parms = list(split = "Gini"))
+m1p1
+prp(m1p1, under = TRUE, branch.lty = 2, yesno = 2, faclen = 0, varlen=15,box.palette = "-RdYlGn")
