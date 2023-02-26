38b7f8d9366aa8d10f8f2f6eebb0326f1fb3a9af
mjcolmenares <124100468+mjcolmenares@users.noreply.github.com>
2023-02-26 11:11:04 -0500

    Update Codigo_Taller2_Parte4_Salario_Pobreza.R

--- scripts/Codigo_Taller2_Parte4_Salario_Pobreza.R
+++ scripts/Codigo_Taller2_Parte4_Salario_Pobreza.R
@@ -18,6 +18,31 @@ rm(list = ls())
 
 # - Librerias y paquetes 
 
-library(pacman)
-p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot, openxlsx, rio)
+p_load(caret, h2o)
 
+train2 <- train 
+test2 <- test 
+
+## Modelos para Predecir los Salarios
+
+# Modelo 1: Regresión Linear
+
+cv1 <- trainControl(number = 5, method = "cv")
+mod1 <- train(Ingtotug~edad + edad_2 + mujer + estudiante + primaria + secundaria + media + superior + exp_trab_actual, 
+              data = train2, 
+              method = "lm",
+              trControl = cv1
+)
+mod1
+
+#Modelo 2: GBM 
+grid_gbm<-expand.grid(n.trees=c(1000),interaction.depth=c(3),shrinkage=c(0.01),n.minobsinnode = c(30))
+gbm_res <- train(Ingtotug~edad + edad_2 + mujer + estudiante + primaria + secundaria + media + superior + exp_trab_actual,
+                      data = train2, 
+                      method = "gbm", 
+                      trControl = cv1,
+                      metric = "RSME",
+                      tuneGrid = grid_gbm
+)
+
+gbm_res$resample$RMSE
