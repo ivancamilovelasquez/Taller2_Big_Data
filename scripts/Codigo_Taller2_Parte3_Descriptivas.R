43edb4bbd9176370813f80ce99152d44c2c98ecf
Jorge Rodríguez <123851986+jorgele054@users.noreply.github.com>
2023-02-26 12:24:13 -0500

    P1 descriptivas

--- scripts/Codigo_Taller2_Parte3_Descriptivas.R
+++ scripts/Codigo_Taller2_Parte3_Descriptivas.R
@@ -21,3 +21,41 @@ rm(list = ls())
 library(pacman)
 p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot, openxlsx, rio)
 
+
+# - 3. Estadísticas Descriptivas 
+
+# - TRAIN
+
+# - Todos 
+
+Base_descriptivas <- train[c("mujer","edad", "amo_casa", "hijos_hogar",
+                           "estudiante", "primaria", 
+                            "secundaria", "media", "superior", 
+                            "Ingtotug", "numero_personas", 
+                            "exp_trab_actual", "horas_trab_usual", 
+                            "num_menores", "Pobre")]
+
+estadisticas_todos <- data.frame(sapply(Base_descriptivas, function(x) 
+  c(mean = mean(x), sd = sd(x))))
+
+write.xlsx(estadisticas_todos, file = "Estadisticos_todos.xlsx")
+
+# - pobres
+
+Base_pobres <- Base_descriptivas[Base_descriptivas$Pobre == 1, ]
+
+estadisticas_pobres <- data.frame(sapply(Base_pobres, function(x) 
+  c(mean = mean(x), sd = sd(x))))
+
+write.xlsx(estadisticas_pobres, file = "Estadisticos_pobres.xlsx")
+
+# - no pobres
+
+Base_nopobres <- Base_descriptivas[Base_descriptivas$Pobre == 0, ]
+
+estadisticas_nopobres <- data.frame(sapply(Base_nopobres, function(x) 
+  c(mean = mean(x), sd = sd(x))))
+
+write.xlsx(estadisticas_nopobres, file = "Estadisticos_nopobres.xlsx")
+
+
