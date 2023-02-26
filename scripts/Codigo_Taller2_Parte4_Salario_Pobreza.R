2fa85b49035eadc0128791c3bd080e920eaf09f8
mjcolmenares <124100468+mjcolmenares@users.noreply.github.com>
2023-02-26 14:05:22 -0500

    Update Codigo_Taller2_Parte4_Salario_Pobreza.R

--- scripts/Codigo_Taller2_Parte4_Salario_Pobreza.R
+++ scripts/Codigo_Taller2_Parte4_Salario_Pobreza.R
@@ -47,31 +47,40 @@ mod2 <- train(Ingtotug~edad + edad_2 + mujer + estudiante + primaria + secundari
 
 mod2
 
+# Modelo 3: Arbol de decision 
+cv5 <- trainControl(number = 5, method = "cv")
+mod3 <- train(Ingtotug~edad+edad_2+mujer+estudiante+primaria+secundaria+
+                   media+superior+exp_trab_actual,
+                 data = train2, 
+                 method = "rpart", 
+                 trControl = cv5)
+mod3
+library(rattle)
+fancyRpartPlot(mod3$finalModel)
 
-# Modelo 3: Random forest y una grilla para tunear 
+# Modelo 4: Random forest y una grilla para tunear 
 tunegrid_rf <- expand.grid(mtry = c(3, 5), 
                            min.node.size = c(10,50,100,150,300),
                            splitrule = "variance")
 
-mod3 <- train(Ingtotug~edad+edad_2+mujer+estudiante+primaria+secundaria+
+mod4 <- train(Ingtotug~edad+edad_2+mujer+estudiante+primaria+secundaria+
                    media+superior+exp_trab_actual,
                  data = train2, 
                  method = "ranger", 
                  trControl = cv5,
                  metric = 'RMSE', 
                  tuneGrid = tunegrid_rf)
-mod3
-plot(mod3)
-
-# Modelo 4: Arbol de decision 
-cv5 <- trainControl(number = 5, method = "cv")
-mod4 <- train(Ingtotug~edad+edad_2+mujer+estudiante+primaria+secundaria+
-                   media+superior+exp_trab_actual,
-                 data = train2, 
-                 method = "rpart", 
-                 trControl = cv5)
 mod4
-library(rattle)
-fancyRpartPlot(modelo1$finalModel)
+plot(mod4)
 
+#Tabla comparativa de los resultados
+Resultados <- matrix(c(mean(mod1$resample$RMSE),mean(mod2$resample$RMSE),mean(mod3$resample$RMSE),mean(mod4$resample$RMSE),
+                  mean(mod1$resample$Rsquared),mean(mod2$resample$Rsquared),mean(mod3$resample$Rsquared),mean(mod4$resample$Rsquared))
+                ,ncol=4 
+                ,byrow=TRUE) 
+colnames(Resultados) <- c("Regresión Linear","Random Forest GBM","Arbol de Decisión","Random Forest")
+rownames(Resultados) <- c("RMSE","R Squared")
 
+Resultados
+
+##Basado en esto, escogeremos el modelo de Random Forest para predecir la pobreza
