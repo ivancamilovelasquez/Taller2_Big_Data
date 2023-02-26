a97f89a53eb3143c8b9ec893463000d8eef36545
Jorge Rodríguez <123851986+jorgele054@users.noreply.github.com>
2023-02-26 14:35:39 -0500

    Gráfico

--- scripts/Codigo_Taller2_Parte3_Descriptivas.R
+++ scripts/Codigo_Taller2_Parte3_Descriptivas.R
@@ -19,7 +19,7 @@ rm(list = ls())
 # - Librerias y paquetes 
 
 library(pacman)
-p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot, openxlsx, rio)
+p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot, openxlsx, rio, GGally)
 
 
 # - 3. Estadísticas Descriptivas 
@@ -74,3 +74,25 @@ estadisticas_todos <- data.frame(sapply(Base_descriptivas, function(x)
   c(mean = mean(x), sd = sd(x))))
 
 write.xlsx(estadisticas_todos, file = "Estadisticos_todos_test.xlsx")
+
+
+# Gráficos
+
+df <- train[c("mujer","edad", 
+                "Ingtotug", "numero_personas", 
+                "exp_trab_actual", "horas_trab_usual", 
+                "num_menores", "Pobre")]
+df$Pobre <- factor(df$Pobre, labels = c("No pobre", "Pobre"))
+
+ggpairs(df, columns = 2:6, ggplot2::aes(colour = Pobre)) +
+  theme_minimal() + 
+  labs(title = "Gráfico de matriz de dispersión") + 
+  scale_y_continuous(labels = scales::number_format()) + 
+  scale_x_continuous(labels = scales::number_format()) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        axis.text = element_text(size = 8))
+
+
+
+
+
